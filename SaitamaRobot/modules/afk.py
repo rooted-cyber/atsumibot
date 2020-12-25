bc6935eb92bdece0c3c57b621c2ea80168d9c573
Rashh <70662641+rash-c9@users.noreply.github.com>
2020-12-25 17:39:43 +0530

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -57,8 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', 'Welcome back you sucker {}','Everyone ready with guns? {} terrorist is here',
-                'AA GYA NALLE WAPAS.... {}', '{} shitss...chutiya aa gya...'
+                'Welcome back you sucker {}','Everyone ready with guns? {} terrorist is here',
+                'AA GYA NALLE WAPAS.... {}', '{} shitss...chutiya aa gya...', 'bsdk {}... why are you back😑',
+                'the bawasir is back!!! {}'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
