1b1db70ae8992324bc71be62a4ede44b89ff2947
Rashh <70662641+rash-c9@users.noreply.github.com>
2020-12-27 01:02:29 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -52,7 +52,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot. You can find my list of available commands with /help....... Owner of bot = [Rashh](https://t.me/rash_sudo).
+I am an Anime themed group management bot. You can find my list of available commands with /help....... maintained by = [Rashh](https://t.me/rash_sudo).
 """
 
 HELP_STRINGS = """
